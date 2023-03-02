28895cd69fb55821a4247bdcb65749ff274230f7
muhsindev0 <muhsinjonmullajonov0@gmail.com>
2023-03-02 12:14:55 +0500

    as

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Site from './vazifa9/Site'
+
+const App = () => {
+  return (
+    <div>
+      <Site/>
+    </div>
+  )
+}
+export default App
+
+
+// 1chi Api 
+// 2chi Lift
